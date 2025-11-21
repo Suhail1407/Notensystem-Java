94bfb0a5ecd6adfd6df161e30cef715d643fea01
Yekda-tech <yekda_tayfur2@gmx.de>
2025-11-21 17:43:20 +0100

    Fach.java

--- Notenverwaltung Projekt/src/Fach.java
+++ Notenverwaltung Projekt/src/Fach.java
@@ -1,16 +1,1 @@
-// Platzhalter für Person 1
-public class Fach {
 
-    // Attribut
-    private String fachName;
-
-    // Konstruktor
-    public Fach(String fachName) {
-        this.fachName = fachName;
-    }
-
-    // Getter-Methode
-    public String getFachName() {
-        return this.fachName;
-    }
-}
