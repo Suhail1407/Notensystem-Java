1595e9fd1af22ba72c24cc329a0346fbd42e8a6d
Yekda-tech <yekda_tayfur2@gmx.de>
2025-11-21 19:16:22 +0100

    Note.java

--- Notenverwaltung Projekt/src/Note.java
+++ Notenverwaltung Projekt/src/Note.java
@@ -1,1 +1,31 @@
+public class Note {
+
+ // Attribute, die benötigt werden 
+  private Fach fach;
+  private double Wert;
+
+
+ // Konsturktor
+   public Note(Fach fach, double Wert){
+     this.fach = fach;
+     this.wert = Wert;
+
+
+
+  
+  // Getter, damit der Benutzer auf die Daten zugreifen kann
+  public String getFach() {
+		return Fach;
+	}
+	public double getWert() {
+		return Wert;
+	}
+	
+  
+
+  
+  
+  
+
+
 
