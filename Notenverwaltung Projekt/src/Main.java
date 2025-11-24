51f22ec0cbe5e048f45288b578c5070c5b206771
Suhail1407 <suhail.haschemi@outlook.com>
2025-11-24 20:02:56 +0100

    Update Main.java

--- Notenverwaltung Projekt/src/Main.java
+++ Notenverwaltung Projekt/src/Main.java
@@ -1,1 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Schueler s1 = new Schueler("JohnIstSchwul", 16);
+
+        // Mehrere Fächer
+        Fach mathe = new Fach("Mathe");
+        Fach deutsch = new Fach("Deutsch");
+        Fach englisch = new Fach("Englisch");
+        Fach physik = new Fach("Physik");
+        Fach informatik = new Fach("Informatik");
+
+        // Ausgabe
+        System.out.println("Schueler: " + s1);
+        System.out.println("Fächer:");
+        System.out.println(mathe);
+        System.out.println(deutsch);
+        System.out.println(englisch);
+        System.out.println(physik);
+        System.out.println(informatik);
+    }
+}
+
 
